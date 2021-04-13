4287b61d6f74657cf5d9b379e9026017805679f4
ysig <y.siglidis@gmail.com>
2021-04-14 01:52:49 +0300

    Update architecture.py

--- architecture.py
+++ architecture.py
@@ -89,7 +89,7 @@ class VisualFeatureEncoder(nn.Module):
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, max_lens):
+    def __init__(self, d_model, max_len):
         super(PositionalEncoding, self).__init__()
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
