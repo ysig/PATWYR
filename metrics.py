ed4b8d4c7954e6ec42ded7c2bd991bb104fb824d
ysig <y.siglidis@gmail.com>
2021-04-16 18:25:40 +0300

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -23,7 +23,7 @@ def ocr_metrics(predicts, ground_truth):
         dist = editdistance.eval(pd_cer, gt_cer)
         cer.append(dist / (max(len(pd_cer), len(gt_cer))))
 
-        print(pd)
+        print(pd, gt)
         pd_wer, gt_wer = pd.split('|'), gt.split('|')
         dist = editdistance.eval(pd_wer, gt_wer)
         wer.append(dist / (max(len(pd_wer), len(gt_wer))))
