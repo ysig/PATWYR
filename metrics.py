95b66fb20f12a35f6ff49aa1fe9c763e7f72adc4
ysig <y.siglidis@gmail.com>
2021-04-16 17:22:30 +0300

    update metrics

--- metrics.py
+++ metrics.py
@@ -1,38 +1,3 @@
-# import numpy as np
-# import jiwer
-# import jiwer.transforms as tr
-
-# class SentencesToListOfCharacters(tr.AbstractTransform):
-#     def process_string(self, s):
-#         return list(s)
-
-#     def process_list(self, inp):
-#         chars = []
-#         for sentence in inp:
-#             chars.extend(self.process_string(sentence))
-#         return chars
-
-# class Bartospace(tr.AbstractTransform):
-#     def process_string(self, s):
-#         return s.replace("|", " ")
-
-#     def process_list(self, inp):
-#         chars = []
-#         for sentence in inp:
-#             chars.extend(self.process_string(sentence))
-#         return chars
-
-
-# class Metrics(object):
-#     def __init__(self):
-#         self.cer_transform = tr.Compose([Bartospace(), tr.RemoveMultipleSpaces(), tr.Strip(), SentencesToListOfCharacters()])
-#         self.wer_transform = tr.Compose([tr.RemoveMultipleSpaces(), tr.Strip()])
-
-#     def cer(self, predictions, references):
-#         return np.mean([jiwer.wer(r, p, truth_transform=self.cer_transform, hypothesis_transform=self.cer_transform) for p, r in zip(predictions, references)])
-
-#     def wer(self, predictions, references):
-#         return np.mean([jiwer.wer(r, p, truth_transform=self.wer_transform, hypothesis_transform=self.wer_transform) for p, r in zip(predictions, references)])
 """
 Copied from https://github.com/him4318/Transformer_ocr/blob/master/src/data/evaluation.py
 Tool to metrics calculation through data and label (string and string).
