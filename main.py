dcfb46f0c8bdf7466d8e16f1235635806b523672
ysig <y.siglidis@gmail.com>
2021-04-20 11:22:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -106,7 +106,11 @@ class Engine(object):
 
         if lr_decay == 'plateau':
             self.adjust_learning_rate(0, lr, None)
-            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optim, mode='min', factor=0.5, patience=5, verbose=True)
+            if dataset[0] == "IAM":
+                patience = 10
+            elif dataset[0] == "Synthetic":
+                patience = 4
+            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optim, mode='min', factor=0.5, patience=patience, verbose=True)
         elif lr_decay is not None:
             decay_rate = float(decay_rate)
 
