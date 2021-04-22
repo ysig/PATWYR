90db8d8c33f4a79abea2b2d1b023ffcd61eaea6c
ysig <y.siglidis@gmail.com>
2021-04-23 00:03:16 +0300

    Update architecture.py

--- architecture.py
+++ architecture.py
@@ -94,11 +94,11 @@ class TransformerHTR(nn.Module):
         x = self.resnet(x)
         b, f, h, w = x.size()        
         x = x.view(b, f*h, w).permute(0, 2, 1)
-        # x = F.relu(self.fc(x))
-        x = self.fc(x)
+        x = F.relu(self.fc(x))
+        # x = self.fc(x)
         x = self.pe_encode(x.permute(1, 0, 2))
-        # x = F.relu(self.fc_bar(x))
-        x = self.fc_bar(x)
+        x = F.relu(self.fc_bar(x))
+        # x = self.fc_bar(x)
         x = self.transformer_encoder(x)
         return x
 
